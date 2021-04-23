51b1c0992c57782cc47f9c765a543d6b6571ad4e
LBrahmadevara <likhithagupthalucky@gmail.com>
2021-04-22 17:58:39 -0700

    working code for Grouped Bar Chart

--- viz_proj/src/frontend/GroupedBarChart/GroupedBarChartTemplate.js
+++ viz_proj/src/frontend/GroupedBarChart/GroupedBarChartTemplate.js
@@ -1,39 +1,28 @@
 import React, { useEffect, useRef } from "react";
-import {
-  select,
-  scaleBand,
-  axisBottom,
-  stack,
-  max,
-  scaleLinear,
-  axisLeft,
-  stackOrderAscending,
-} from "d3";
+import { select, scaleBand, axisBottom, max, scaleLinear, axisLeft } from "d3";
 
-const StackedBarChartTemplate = ({ data, keys, colors }) => {
+const GroupedBarChartTemplate = ({ data, keys, colors }) => {
   const svgRef = useRef();
-
   useEffect(() => {
     const svg = select(svgRef.current);
 
-    // Stacks / layers
-    const stackGenerator = stack().keys(keys).order(stackOrderAscending);
-    const layers = stackGenerator(data);
-    const extent = [
-      0,
-      max(layers, (layer) => max(layer, (sequence) => sequence[1])) + 500,
-    ];
+    const extent = [0, max(data, (dict) => max(keys, (i) => dict[i]))];
+    console.log(extent);
 
-    // Scaling
-    const xScale = scaleBand()
-      .domain(data.map((d) => d.Month))
+    //Scaling
+    const xScale0 = scaleBand()
+      .domain(data.map((item) => item.Month))
       .range([0, 1000])
-      .padding(0.45);
+      .padding(0.1);
+    const xScale1 = scaleBand()
+      .domain(keys)
+      .range([0, xScale0.bandwidth()])
+      .padding(0.1);
 
     const yScale = scaleLinear().domain(extent).range([300, 0]);
 
     // Axis
-    const xAxis = axisBottom(xScale);
+    const xAxis = axisBottom(xScale0);
     svg.select(".x-axis").style("transform", "translateY(300px)").call(xAxis);
 
     const yAxis = axisLeft(yScale).ticks(4);
@@ -51,22 +40,27 @@ const StackedBarChartTemplate = ({ data, keys, colors }) => {
 
     svg
       .selectAll(".layer")
-      .data(layers)
+      .data(data)
       .join("g")
       .attr("class", "layer")
-      .attr("fill", (layer) => colors[layer.key])
+      .attr("transform", (d) => `translate(${xScale0(d.Month)}, 0)`)
       .selectAll("rect")
-      .data((layer) => layer)
+      .data((d) =>
+        keys.map(function (key) {
+          return { key: key, value: d[key] };
+        })
+      )
       .join("rect")
-      .attr("x", (sequence) => xScale(sequence.data.Month))
-      .attr("width", xScale.bandwidth())
+      .attr("x", (d) => xScale1(d.key))
+      .attr("width", xScale1.bandwidth())
+
       .on("mouseenter", (event, value) => {
         svg
           .selectAll(".tooltip")
-          .data(value)
+          .data([value["value"]])
           .join((enter) => enter.append("text"))
           .attr("class", "tooltip")
-            // .text(value[1])
+          .text([value["value"]])
           .attr("text-anchor", "middle")
           .transition()
           .attr("font-size", 18)
@@ -76,16 +70,17 @@ const StackedBarChartTemplate = ({ data, keys, colors }) => {
       .on("mousemove", (event, value) => {
         svg
           .select(".tooltip")
-          .text(`$${value[1]}`)
+          .text(`$${value.value}`)
           .attr("x", event.offsetX + 50 + "px")
           .attr("y", event.offsetY - 1 + "px");
       })
 
       .on("mouseleave", () => svg.select(".tooltip").remove())
 
-      .attr("y", (sequence) => yScale(sequence[1]))
-      .attr("height", (sequence) => yScale(sequence[0]) - yScale(sequence[1]));
-  }, [data, keys, colors]);
+      .attr("y", (d) => yScale(d.value))
+      .attr("height", (d) => 300 - yScale(d.value))
+      .attr("fill", (d) => colors[d.key]);
+  });
   return (
     <svg
       ref={svgRef}
@@ -98,4 +93,4 @@ const StackedBarChartTemplate = ({ data, keys, colors }) => {
   );
 };
 
-export default StackedBarChartTemplate;
+export default GroupedBarChartTemplate;
